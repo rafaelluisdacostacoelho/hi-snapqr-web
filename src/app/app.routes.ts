873c07e2c919372e79cd8083cca1918407f42251
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2025-01-19 17:59:45 -0300

    Translate privacy policy.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { PacksComponent } from './packs/packs.component';
 import { HelpComponent } from './help/help.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ContactComponent } from './contact/contact.component';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
 export const routes: Routes = [{
     path: "qr-code-generator", component: QrCodeGeneratorComponent
@@ -14,8 +16,12 @@ export const routes: Routes = [{
     path: "help", component: HelpComponent
 }, {
     path: "sign-in", component: SignInComponent
-},{
+}, {
     path: "privacy-policy", component: PrivacyPolicyComponent
+}, {
+    path: "terms-of-service", component: TermsOfServiceComponent
+}, {
+    path: "contact", component: ContactComponent
 }, {
     path: "**", component: PresentationComponent
 }];
