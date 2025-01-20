2ece5a14dd7568a7b0c027e1c58e0dfc0ac2bb63
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2025-01-19 23:46:55 -0300

    Adds pix generator and terms of service.

--- src/app/qr-code-generator/qr-code-generator.component.ts
+++ src/app/qr-code-generator/qr-code-generator.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit }
+  from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-qr-code-generator',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './qr-code-generator.component.html',
   styleUrl: './qr-code-generator.component.scss'
 })
 export class QrCodeGeneratorComponent {
+  public keyPix: string = '';
+  public loading: boolean = false;
+  public imageBase64 = '';
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    this.loading = true;
+    this.http.post(environment.url, {
+      PixKey: this.keyPix
+    }).subscribe({
+      next: (response: any) => {
+        this.imageBase64 = response.qrCodeBase64;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Erro ao gerar QRCode:', error); this.loading = false;
+      }
+    });
+  }
 }
